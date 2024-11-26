097a5f41957f1e9f8ed2145ebf7aec31577c287f
Sheehan Shams <s2shams@uwaterloo.ca>
2024-11-26 00:53:02 -0500

    moved to src folder

---
+++ src/Graphics.h
@@ -0,0 +1,25 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+#include "Observer.h"
+#include "window.h"
+#include "Game.h"
+#include <vector>
+#include <memory>
+
+enum class Colour {White, Black, Red, Blue, Green, Yellow, Gray};
+
+class Graphics : public Observer {
+    weak_ptr<Game> g;
+    int playerNumber;
+    bool multipleDisplay;
+    Xwindow w;
+    public:
+    Graphics(weak_ptr<Game> &game, int playerNumber, bool multipleDisplay);
+    void notify() override;
+    void displayAbilities();
+    void displayError(const string &Err);
+    int getPlayerNumber() const;
+};
+
+
+#endif
