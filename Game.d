fb9b802cff9fe430defa07eee8b44126375a3f58
s-sharar <ssharar@uwaterloo.ca>
2024-11-26 00:39:47 -0500

    Updated bonus functionality + controller class

--- Game.d
+++
@@ -1,5 +0,0 @@
-Game.o: Game.cc Game.h Cell.h Player.h Link.h Subject.h Board.h Err.h \
- Ability.h Graphics.h Observer.h window.h \
- /opt/homebrew/include/X11/Xlib.h /opt/homebrew/include/X11/X.h \
- /opt/homebrew/include/X11/Xfuncproto.h \
- /opt/homebrew/include/X11/Xosdefs.h TextDisplay.h
